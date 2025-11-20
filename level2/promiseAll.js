271ca9bda5c78b1281a465e8b5945a089bfcab23
TheHarald <r.surinov@netris.ru>
2025-11-20 18:55:19 +0300

    fix tasks

--- level2/promiseAll.js
+++ level2/promiseAll.js
@@ -1,8 +1,8 @@
 const tasks = [
-    () => new Promise(res => setTimeout(() => res('Task 1'), 1000)),
-    () => new Promise(res => setTimeout(() => res('Task 2'), 2000)),
-    () => new Promise(res => setTimeout(() => res('Task 3'), 500)),
-    () => new Promise(res => setTimeout(() => res('Task 4'), 1500)),
+    new Promise(res => setTimeout(() => res('Task 1'), 1000)),
+    new Promise(res => setTimeout(() => res('Task 2'), 2000)),
+    new Promise(res => setTimeout(() => res('Task 3'), 500)),
+    new Promise(res => setTimeout(() => res('Task 4'), 1500)),
 ];
 
 
--- level2/promisePool.js
+++ level2/promisePool.js
@@ -4,10 +4,10 @@
  */
 
 const tasks = [
-    () => new Promise(res => setTimeout(() => res('Task 1'), 1000)),
-    () => new Promise(res => setTimeout(() => res('Task 2'), 2000)),
-    () => new Promise(res => setTimeout(() => res('Task 3'), 500)),
-    () => new Promise(res => setTimeout(() => res('Task 4'), 1500)),
+    new Promise(res => setTimeout(() => res('Task 1'), 1000)),
+    new Promise(res => setTimeout(() => res('Task 2'), 2000)),
+    new Promise(res => setTimeout(() => res('Task 3'), 500)),
+    new Promise(res => setTimeout(() => res('Task 4'), 1500)),
 ];
 
 promisePool(tasks, 2).then(results => {
