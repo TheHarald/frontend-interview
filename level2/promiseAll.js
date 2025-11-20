26129712a957027064a663fa66ca366db299f488
TheHarald <r.surinov@netris.ru>
2025-11-20 17:48:57 +0300

    fix

--- level2/promiseAll.js
+++ level2/promiseAll.js
@@ -1,1 +1,13 @@
-// todo
+const tasks = [
+    () => new Promise(res => setTimeout(() => res('Task 1'), 1000)),
+    () => new Promise(res => setTimeout(() => res('Task 2'), 2000)),
+    () => new Promise(res => setTimeout(() => res('Task 3'), 500)),
+    () => new Promise(res => setTimeout(() => res('Task 4'), 1500)),
+];
+
+
+function promiseAll(promises) { }
+
+promiseAll(tasks).then(results => {
+    console.log(results); // ['Task 1', 'Task 2', 'Task 3', 'Task 4']
+});
