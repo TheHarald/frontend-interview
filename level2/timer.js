7283b6f07fa4308482572606a930af17f646e7de
TheHarald <r.surinov@netris.ru>
2025-11-21 14:47:37 +0300

    upd timer task

--- level2/timer.js
+++ level2/timer.js
@@ -1,11 +1,21 @@
-/**
- * Реализуйте класс таймера с паузой/возобновлением
- */
+class Timer { }
 
-const timer = new CountdownTimer(60); // 60 секунд
+// Пример использования:
+const timer = new Timer(60); // 60 секунд
+
+timer.onTick = (seconds) => console.log(`Осталось: ${seconds} сек`);
+timer.onComplete = () => console.log('Время вышло!');
 
 timer.start();
-timer.pause();
-timer.resume();
-timer.onTick = (seconds) => console.log(seconds);
-timer.onComplete = () => console.log('Время вышло!');
+
+// Через 10 секунд поставим на паузу
+setTimeout(() => {
+    timer.pause();
+    console.log('Таймер на паузе');
+
+    // Через 5 секунд возобновим
+    setTimeout(() => {
+        timer.resume();
+        console.log('Таймер возобновлен');
+    }, 5000);
+}, 10000);
