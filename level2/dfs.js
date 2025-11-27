632c04f552796fbb0ad2be680ba11bd6213f92b3
TheHarald <r.surinov@netris.ru>
2025-11-27 22:14:17 +0300

    upd dfs

--- level2/dfs.js
+++ level2/dfs.js
@@ -2,7 +2,7 @@
  * Напишите функцию для поиска значения в дереве с помощью DFS
  */
 function dfs(tree, target) {
-    // Ваш код здесь
+
 }
 
 const tree = {
@@ -13,9 +13,24 @@ const tree = {
             value: 3, children: [
                 { value: 4, children: [] }
             ]
-        }
+        },
+        {
+            value: 15, children: [
+                { value: 6, children: [] },
+                {
+                    value: 7, children: [
+                        { value: 8, children: [] },
+                    ]
+                },
+            ]
+        },
     ]
 };
 
 console.log(dfs(tree, 4)); // true
 console.log(dfs(tree, 5)); // false
+console.log(dfs(tree, 8)); // true
+console.log(dfs(tree, 1));  // true (корень)
+console.log(dfs(tree, 2));  // true (первый ребенок)
+console.log(dfs(tree, 15)); // true (узел с детьми)
+console.log(dfs(tree, 9));  // false (не существует)
